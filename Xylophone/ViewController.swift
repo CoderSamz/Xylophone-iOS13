b53f98e5b76643d009e820dcf5ee319cb3e93ed8
CoderSamz <codersamz-dev@outlook.com>
2022-07-30 18:11:27 +0800

    77. Boss Challenge 大挑战

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        // delay 0.2s
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+
         if let buttonTitle = sender.currentTitle {
             playSound(soundName: buttonTitle)
         }
